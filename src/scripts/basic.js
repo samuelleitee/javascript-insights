48142b332f81a5c6ba6aab87abb079e67b28b8f4
samuelleitee <programmer.and.investor@gmail.com>
2024-03-28 21:51:09 -0300

    feat: add basic concepts of JavaScript commented

---
+++ src/scripts/basic.js
@@ -0,0 +1,66 @@
+/* VARIÁVEIS
+ *
+ * var - escopo global | pode ser reatribuída e redeclarada
+ * let - escopo de bloco | pode ser reatribuída, mas não redeclarada
+ * const - escopo de bloco | não pode ser reatribuída nem redeclarada
+ * 
+ */
+
+/* TIPOS DE DADOS
+ *
+ * number
+ * string
+ * boolean
+ * function
+ * object
+ * 
+ * undefined
+ * null
+ * 
+ */
+
+/* OPERADORES ARITMÉTICOS
+ *
+ * + | adição
+ * - | subtração
+ * * | multiplicação
+ * / | divisão
+ * % | módulo (resto de uma operação de divisão)
+ * ++ | incremento
+ * -- | decremento
+ * 
+ */
+
+/* OPERADORES DE ATRIBUIÇÃO
+ *
+ * = | atribuição
+ * += | atribuição de soma (x += y) == (x = x + y)
+ * -= | atribuição de subtração (x -= y) == (x = x - y)
+ * *= | atribuição de multiplicação (x *= y) == (x = x * y)
+ * /= | atribuição de divisão (x /= y) == (x = x / y)
+ * %= | atribuição de módulo/resto (x %= y) == (x = x % y)
+ * 
+ */
+
+/* OPERADORES DE COMPARAÇÃO
+ *
+ * == | igual a
+ * === | igual a (tanto o valor quanto o tipo do objeto)
+ * != | diferente de
+ * > | maior que
+ * >= | maior ou igual a 
+ * < | menor que
+ * <= | menor ou igual a 
+ * 
+ */
+
+/* OPERADORES DE BIT A BIT (BITWISE)
+ *
+ * & | e
+ * | | ou
+ * ~ | negação
+ * ^ | ou exclusivo (XOR)
+ * << | deslocamento para a esquerda (left shift)
+ * >> | deslocamento para a direita (right shift)
+ * 
+ */
