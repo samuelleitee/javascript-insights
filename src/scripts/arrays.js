8df005dc3fbee4632ff5b49ac0e21fc48d8f13be
samuelleitee <programmer.and.investor@gmail.com>
2024-03-28 21:51:26 -0300

    feat: add arraysconcepts of JavaScript commented

---
+++ src/scripts/arrays.js
@@ -0,0 +1,28 @@
+/* ARRAYS
+ *
+ * Um array armazena valores do mesmo tipo, em sequência. Apesar de o JavaScript permitir criar arrays com valores de tipos distintos, não se trata de uma boa prática.
+ *
+ * Sintaxe: var array = [value, value, value]
+ *
+ * métodos:
+ *   array[position] - acessa o valor contido na posição especificada.
+ *   length - retorna o número de valores existentes em um array.
+ *   push - adiciona valores ao final de um array.
+ *   unshift - adiciona valores no início de um array
+ *
+ *   concat
+ *   every
+ *   filter
+ *   forEach
+ *   join
+ *   indexOf
+ *   lastIndexOf
+ *   map
+ *   reverse
+ *   slice
+ *   some
+ *   sort
+ *   toString
+ *   valueOf
+ *
+ */
